cb3a1a07fde918c889db424a594e3bf0d750f257
qw0te <baert.quentin@gmail.com>
2014-09-23 14:48:50 +0200

    First implementation of the QueryBuilder class

--- src/query/QueryBuilder.java
+++ src/query/QueryBuilder.java
@@ -1,10 +1,46 @@
 package query;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 public class QueryBuilder {
 
-    public QueryBuilder() {
-        // TODO Auto-generated constructor stub
-    }
+	/* Fields */
 
+	/**
+	 * Twitter singleton.
+	 */
+	private final Twitter TWITTER = TwitterFactory.getSingleton();
+
+	/**
+	 * String representing the search query.
+	 */
+	private String searchQuery;
+
+	/* Methods */
+
+	/**
+	 * Constructor of the QueryBuilder class.
+	 * 
+	 * @param searchQuery
+	 *            string representing the search query
+	 */
+	public QueryBuilder ( String searchQuery ) {
+		this.searchQuery = searchQuery;
+	}
+	
+	/**
+	 * Gets the results of the query.
+	 * 
+	 * @return results of the query.
+	 * @throws TwitterException //TODO
+	 */
+	public QueryResult getResults () throws TwitterException {
+		Query query = new Query( this.searchQuery );
+
+		return TWITTER.search( query );
+	}
 }
