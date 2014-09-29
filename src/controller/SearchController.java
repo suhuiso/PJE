64cf9d8f6f004a643ca759dcd1b5a26bb1e3e61e
Ath0m <thomas.t.bernard@gmail.com>
2014-09-30 00:00:08 +0200

    SearchController added

---
+++ src/controller/SearchController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import model.AbstractModel;
+
+public class SearchController extends AbstractController {
+
+	public SearchController (AbstractModel model) {
+		super( model );
+	}
+
+	@Override
+	void control() {
+		// TODO Validation du champ de recherche
+	}
+
+}
