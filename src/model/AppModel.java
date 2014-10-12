8d00ed639c223e63e82eb700670813a4f5ce1d7d
qw0te <baert.quentin@gmail.com>
2014-10-12 23:05:51 +0200

    -m "Addition of a TweetPool in the model"

--- src/model/AppModel.java
+++ src/model/AppModel.java
@@ -11,10 +11,17 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import utils.TweetPool;
 
 public class AppModel extends Observable {
 	
 	private final Twitter TWITTER = TwitterFactory.getSingleton();
+	
+	private TweetPool tweetPool;
+	
+	public AppModel () {
+		this.tweetPool = new TweetPool("resources/tweetPool.csv");
+	}
 
 	public void search ( String searchQuery ) {
 		Query query = new Query( searchQuery );
