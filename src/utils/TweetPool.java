401aded61909a2df9ea3fe7f6d223ace85ac53ba
qw0te <baert.quentin@gmail.com>
2014-10-12 12:13:44 +0200

    TweetPool initialization from a CSV file

--- src/utils/TweetPool.java
+++ src/utils/TweetPool.java
@@ -1,19 +1,54 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
-
-import twitter4j.Status;
 
 /**
  * // TODO
- * // SINGLETON 
+ * // SINGLETON
  */
 public class TweetPool {
 
-	private HashMap< Long, Status > tweetPool;
+	private HashMap< Long, Tweet > tweetPool;
 
-	public TweetPool ( HashMap< Long, Status > tweetPool ) {
-		this.tweetPool = tweetPool;
+	public TweetPool ( String path ) {
+		this.tweetPool = new HashMap< Long, Tweet >();
+
+		try {
+			BufferedReader br = new BufferedReader( new FileReader( path ) );
+			String line;
+
+			while ( ( line = br.readLine() ) != null ) {
+				String[] elem = line.split( ";" );
+				// NOT SURE
+				SimpleDateFormat formatter = new SimpleDateFormat( "EEEE, MMM dd, yyyy HH:mm:ss a" );
+
+				Tweet tweet =
+				        new Tweet( Long.parseLong( elem[ 0 ] ), elem[ 1 ], elem[ 2 ],
+				                formatter.parse( elem[ 3 ] ), elem[ 4 ] );
+
+				this.tweetPool.put( tweet.getId(), tweet );
+			}
+
+			br.close();
+		} catch ( FileNotFoundException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch ( IOException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch ( NumberFormatException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch ( ParseException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
