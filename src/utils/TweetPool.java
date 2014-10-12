c96af58e26522b9f0a9de57d9cf5cabc67f61209
qw0te <baert.quentin@gmail.com>
2014-10-12 23:01:01 +0200

    -m "A test is done to know if a csv file already exists before trying to fill the tweet pool"

--- src/utils/TweetPool.java
+++ src/utils/TweetPool.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,36 +19,40 @@ public class TweetPool {
 
 	public TweetPool ( String path ) {
 		this.tweetPool = new HashMap< Long, Tweet >();
+		File file = new File( path );
 
-		try {
-			BufferedReader br = new BufferedReader( new FileReader( path ) );
-			String line = "";
+		if ( file.exists() && !file.isDirectory() ) {
+			try {
+				BufferedReader br = new BufferedReader( new FileReader( path ) );
+				String line = "";
 
-			while ( ( line = br.readLine() ) != null ) {
-				String[] elem = line.split( ";" );
-				// NOT SURE
-				SimpleDateFormat formatter = new SimpleDateFormat( "EEEE, MMM dd, yyyy HH:mm:ss a" );
+				while ( ( line = br.readLine() ) != null ) {
+					String[] elem = line.split( ";" );
+					// NOT SURE
+					SimpleDateFormat formatter =
+					        new SimpleDateFormat( "EEEE, MMM dd, yyyy HH:mm:ss a" );
 
-				Tweet tweet =
-				        new Tweet( Long.parseLong( elem[ 0 ] ), elem[ 1 ], elem[ 2 ],
-				                formatter.parse( elem[ 3 ] ), elem[ 4 ] );
+					Tweet tweet =
+					        new Tweet( Long.parseLong( elem[ 0 ] ), elem[ 1 ], elem[ 2 ],
+					                formatter.parse( elem[ 3 ] ), elem[ 4 ] );
 
-				this.tweetPool.put( tweet.getId(), tweet );
+					this.tweetPool.put( tweet.getId(), tweet );
+				}
+
+				br.close();
+			} catch ( FileNotFoundException e ) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch ( IOException e ) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch ( NumberFormatException e ) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch ( ParseException e ) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-
-			br.close();
-		} catch ( FileNotFoundException e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch ( IOException e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch ( NumberFormatException e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch ( ParseException e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 
