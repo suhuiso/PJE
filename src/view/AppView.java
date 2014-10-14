b258a30d64eb544f657d56161e4b7197abeb1eee
Ath0m <thomas.t.bernard@gmail.com>
2014-10-14 10:15:25 +0200

    Graphic Review : New main view backbone (real cleaned)

--- src/view/AppView.java
+++ src/view/AppView.java
@@ -22,29 +22,29 @@ public class AppView extends JFrame {
 	public AppView () {
 
 		/* Title of the window */
-		this.setTitle("AwesomeName");
+		this.setTitle( "AwesomeName" );
 
 		/* Size of the window, in pixels */
-		this.setSize(400, 600);
+		this.setSize( 400, 600 );
 
 		/* Minimal size to avoid shrinking of the window */
-		this.setMinimumSize(new Dimension(400, 600));
+		this.setMinimumSize( new Dimension( 400, 600 ) );
 
 		/* App launch at the middle of the screen */
-		this.setLocationRelativeTo(null);
+		this.setLocationRelativeTo( null );
 
 		/* App closes when close button is pressed */
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
 		/* Window is made visible */
-		this.setVisible(true);
+		this.setVisible( true );
 
 		/*
 		 * CONTENT :
 		 * 
 		 * AppPanel : Main Panel of the window
 		 */
-		this.setContentPane(new AppPanel());
+		this.setContentPane( new AppPanel() );
 	}
 
 }
