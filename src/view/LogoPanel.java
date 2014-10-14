c84b63935cf50655422baa2edd245b5817766616
Ath0m <thomas.t.bernard@gmail.com>
2014-10-14 11:16:34 +0200

    Graphic Review : Logo Panel added

---
+++ src/view/LogoPanel.java
@@ -0,0 +1,59 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+/**
+ * Logo panel of the application.
+ * 
+ * @author Quentin Baert & Thomas Bernard
+ */
+@SuppressWarnings("serial")
+public class LogoPanel extends JPanel {
+
+	////////////
+	// FILEDS //
+	////////////
+
+	/**
+	 * Image used to display the logo
+	 */
+	private BufferedImage image;
+
+	/////////////
+	// METHODS //
+	/////////////
+
+	/**
+	 * Constructor of the LogoPanel
+	 */
+	public LogoPanel() {
+		super();
+
+		try {
+			/* Access to the file containing the image */
+			image = ImageIO.read( new File( "search.png" ) );
+		} catch ( IOException e ) {
+			/* File can't be loaded */
+			System.err.println( "ERROR: Fail to load search.png file" );
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		/* Image is drawn and horizontally centered */
+		g.drawImage( image, this.getWidth()/2 - image.getWidth( null )/2 , 0, this );
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension( 384, 70 );
+	}
+}
