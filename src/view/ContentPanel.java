5eeef4d932ef967d25d82b26602b0740c43efe73
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-04 12:12:44 +0100

    link the content to the list of tweets

--- src/view/ContentPanel.java
+++ src/view/ContentPanel.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JPanel;
@@ -26,5 +27,14 @@ public class ContentPanel extends JPanel {
 		
 		this.setBackground( Color.WHITE );						/* color: white */
 		this.setBorder( new EmptyBorder( 10, 10, 10, 10 ) );	/* padding: 10px */
+		
+		this.setLayout( new BorderLayout() );
+		
+		/*
+		 * CONTENT :
+		 * 
+		 * ListTweetPanel : List of tweets
+		 */
+		this.add( new ListTweetPanel(), BorderLayout.CENTER );
 	}
 }
