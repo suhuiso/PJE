04e38d2b347d90a03b370dfe2c870dbcc0ea6db8
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-18 11:58:53 +0100

    MVC-UI : update TweetPanel

--- src/view/TweetPanel.java
+++ src/view/TweetPanel.java
@@ -1,28 +1,60 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import twitter4j.Status;
+import controller.AppController;
 
 @SuppressWarnings("serial")
 public class TweetPanel extends JPanel {
 
+	////////////
+	// FIELDS //
+	////////////
+	
+	/**
+	 * Controller of the TweetPanel
+	 */
+	private AppController controller;
+
+	/**
+	 * Status of the tweet
+	 */
+	private Status status;
+	
 	/////////////
 	// METHODS //
 	/////////////
-
+	
 	/**
 	 * Constructor of the TweetPanel
+	 * 
+	 * @param controller
+	 *            controller of the TweetPanel
 	 */
-	public TweetPanel() {
+	public TweetPanel( AppController controller, Status status ) {
 		super();
 
-		this.setBackground( new Color( 0x55ACEE ) );		/* color: blue */
+		this.setBackground( Color.WHITE );		/* color: white */
+		
+		this.setLayout( new BorderLayout() );
+		
+		/* Controller of TweetPanel is added */
+		this.controller = controller;
+		/* Status of this tweet is added */
+		this.status = status;
+		
+		this.add( new JLabel( "@" + status.getUser().getScreenName() ), BorderLayout.NORTH );
+		this.add( new JLabel( status.getText() ), BorderLayout.CENTER );
 	}
 
 	@Override
 	public Dimension getPreferredSize() {
-		return new Dimension( 50, 50 );
+		return new Dimension( 400, 60 );
 	}
 }
