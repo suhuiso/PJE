62aac34cc89c4e7408cf9d6a88f2303f15a65ac1
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-18 11:59:19 +0100

    MVC-UI : update Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,15 +1,18 @@
 package app;
 
+import model.AppModel;
 import view.AppViewTmp;
-import model.*;
-import controller.*;
+import controller.AppController;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		AppModel search = new AppModel();
 		AppController controller = new AppController( search );
+		/* First view */
 		AppViewTmp searchView = new AppViewTmp( controller );
+		/* New view : Work in progress */
+		//AppView searchView = new AppView( controller );
 		
 		search.addObserver( searchView );
 	}	
