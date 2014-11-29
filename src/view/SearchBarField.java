b104278e78d888319513d8cd137f24cab6799b7e
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-29 19:37:18 +0100

    update SearchBarField

--- src/view/SearchBarField.java
+++ src/view/SearchBarField.java
@@ -2,35 +2,23 @@ package view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-/**
- * Search bar field of header panel.
- * 
- * @author Quentin Baert & Thomas Bernard
- */
 @SuppressWarnings("serial")
 public class SearchBarField extends JTextField {
 
-	/////////////
-	// METHODS //
-	/////////////
-
-	/**
-	 * Constructor of the SearchBarField
-	 */
 	public SearchBarField () {
 		super();
-
-		this.setBackground( Color.WHITE );					/* color: white */
-		this.setBorder( new EmptyBorder( 0, 10, 0, 10 ) );	/* padding: 0 */
+		this.setBackground( new Color( 0xEEEEEE ) );
+		this.setBorder( new EmptyBorder( 0, 20, 0, 20 ) );
+		this.setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
 	}
 	
 	@Override
 	public Dimension getPreferredSize() {
-		return new Dimension( 300, 30 );
+		return new Dimension( 580, 100 );
 	}
-	
 }
