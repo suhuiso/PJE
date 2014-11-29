44b47fd309b54d48e620c4c611f5624039d92053
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-29 16:55:58 +0100

    compact AppPanel source code

--- src/view/AppPanel.java
+++ src/view/AppPanel.java
@@ -10,65 +10,25 @@ import javax.swing.border.EmptyBorder;
 
 import controller.AppController;
 
-/**
- * Main panel of the application.
- * 
- * @author Quentin Baert & Thomas Bernard
- */
 @SuppressWarnings("serial")
-public class AppPanel extends JPanel implements Observer {
+public class AppPanel extends JPanel {
 
-	////////////
-	// FIELDS //
-	////////////
-	
-	/**
-	 * Controller of the AppPanel
-	 */
 	private AppController controller;
-	
-	/**
-	 * Header Panel
-	 */
-	private HeaderPanel headerPanel;
-	
-	/**
-	 * Content Panel
-	 */
 	private SidebarPanel sidebarPanel;
 	private ContentPanel contentPanel;
 	
-	/////////////
-	// METHODS //
-	/////////////
-
-	/**
-	 * Constructor of the AppPanel
-	 * 
-	 * @param controller
-	 *            controller of the AppPanel
-	 */
 	public AppPanel ( AppController controller ) {
 		super();
-		
-		this.setBackground( new Color( 0x55ACEE ) );		/* color: blue */
-		this.setBorder( new EmptyBorder( 8, 8, 8, 8 ) );	/* padding : 8px */
+		this.setBackground( Color.WHITE );
+		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
 
 		this.setLayout( new BorderLayout() );
 		
-		/* Controller of AppPanel is added */
 		this.controller = controller;
 		
-		/* Add content fields */
 		this.sidebarPanel = new SidebarPanel( controller );
 		this.contentPanel = new ContentPanel( controller );
 		
-		/*
-		 * CONTENT :
-		 * 
-		 * HeaderPanel : Header of the application
-		 * ContentPanel : Main content 
-		 */
 		this.add( this.sidebarPanel, BorderLayout.WEST );
 		this.add( this.contentPanel, BorderLayout.CENTER );
 	}
