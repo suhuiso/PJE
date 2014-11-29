219f38d42b0eff76cb8c0b99248ce8e72584a538
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-29 19:38:04 +0100

    add a search bar in TendenciesPanel

--- src/view/TendenciesPanel.java
+++ src/view/TendenciesPanel.java
@@ -1,6 +1,8 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -12,9 +14,26 @@ public class TendenciesPanel extends JPanel {
 
     public static final String CARD_TENDENCIES =  "Tendencies Panel";
     public static final Color TENDENCIES_COLOR = new Color( 0x2ECC71 );
+    
+	private AppController controller;
+    private SearchBarPanel searchBarPanel;
+    
 	public TendenciesPanel( AppController controller ) {
 		super();		
-		this.setBackground( new Color( 0x2ECC71 ) );
+		this.setBackground( TendenciesPanel.TENDENCIES_COLOR );
 		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		
+		this.setLayout( new BorderLayout() );
+		
+		this.controller = controller;
+		
+		this.searchBarPanel = new SearchBarPanel( controller, TendenciesPanel.TENDENCIES_COLOR );
+		
+		this.add( this.searchBarPanel, BorderLayout.NORTH );
+	}
+	
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension( 800, 600 );
 	}
 }
