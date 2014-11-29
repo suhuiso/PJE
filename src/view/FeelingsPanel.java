81b3768b4edc4cf1b9e367a75b4e8cdec8eae55c
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-29 19:37:46 +0100

    add a search bar in FeelingsPanel

--- src/view/FeelingsPanel.java
+++ src/view/FeelingsPanel.java
@@ -1,6 +1,9 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -12,10 +15,26 @@ public class FeelingsPanel extends JPanel {
 
     public static final String CARD_FEELINGS =  "Feelings Panel";
     public static final Color FEELINGS_COLOR = new Color( 0x0E83CD );
+    
+	private AppController controller;
+    private SearchBarPanel searchBarPanel;
 	
 	public FeelingsPanel( AppController controller ) {
 		super();		
-		this.setBackground( new Color( 0x0E83CD ) );
+		this.setBackground( FeelingsPanel.FEELINGS_COLOR );
 		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		
+		this.setLayout( new BorderLayout() );
+		
+		this.controller = controller;
+		
+		this.searchBarPanel = new SearchBarPanel( controller, FeelingsPanel.FEELINGS_COLOR );
+		
+		this.add( this.searchBarPanel, BorderLayout.NORTH );
+	}
+	
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension( 800, 600 );
 	}
 }
