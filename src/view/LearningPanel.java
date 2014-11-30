102b52638df271e9e0fb30c55fa109fe28578351
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-30 14:56:12 +0100

    update with AbstractCardPanel

--- src/view/LearningPanel.java
+++ src/view/LearningPanel.java
@@ -2,48 +2,37 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.util.Observable;
-import java.util.Observer;
-
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import controller.AppController;
 
 @SuppressWarnings("serial")
-public class LearningPanel extends JPanel implements Observer {
+public class LearningPanel extends AbstractCardPanel {
 
     public static final String CARD_LEARNING =  "Learning Panel";
     public static final Color LEARNING_COLOR = new Color( 0xFCD04B );
     
-	private AppController controller;
     private SearchBarPanel searchBarPanel;
 	private ListTweetPanel listTweetPanel;
     
 	public LearningPanel( AppController controller ) {
-		super();		
+		super( controller );
 		this.setBackground( LearningPanel.LEARNING_COLOR );
-		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
-		
-		this.setLayout( new BorderLayout() );
-		
-		this.controller = controller;
-		
+
 		this.searchBarPanel = new SearchBarPanel( controller, LearningPanel.LEARNING_COLOR );
 		this.listTweetPanel = new ListTweetPanel( controller, LearningPanel.LEARNING_COLOR );
-		
+
 		this.add( this.searchBarPanel, BorderLayout.NORTH );
 		this.add( this.listTweetPanel, BorderLayout.CENTER );
-	}
-	
-	@Override
-	public Dimension getPreferredSize() {
-		return new Dimension( 800, 600 );
 	}
 
 	@Override
 	public void update ( Observable o, Object arg ) {
 		this.listTweetPanel.update( o, arg );
+	}
+
+	@Override
+	public void clear () {
+		// TODO Auto-generated method stub
 	}
 }
