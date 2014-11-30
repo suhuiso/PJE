896458db9f69e350c6d477b297b52d196dda54a1
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-30 14:56:07 +0100

    update with AbstractCardPanel

--- src/view/TendenciesPanel.java
+++ src/view/TendenciesPanel.java
@@ -2,38 +2,34 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import java.util.Observable;
 
 import controller.AppController;
 
 @SuppressWarnings("serial")
-public class TendenciesPanel extends JPanel {
+public class TendenciesPanel extends AbstractCardPanel {
 
     public static final String CARD_TENDENCIES =  "Tendencies Panel";
     public static final Color TENDENCIES_COLOR = new Color( 0x2ECC71 );
     
-	private AppController controller;
     private SearchBarPanel searchBarPanel;
     
 	public TendenciesPanel( AppController controller ) {
-		super();		
+		super( controller );		
 		this.setBackground( TendenciesPanel.TENDENCIES_COLOR );
-		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
-		
-		this.setLayout( new BorderLayout() );
-		
-		this.controller = controller;
-		
+						
 		this.searchBarPanel = new SearchBarPanel( controller, TendenciesPanel.TENDENCIES_COLOR );
 		
 		this.add( this.searchBarPanel, BorderLayout.NORTH );
 	}
-	
+
 	@Override
-	public Dimension getPreferredSize() {
-		return new Dimension( 800, 600 );
+	public void update ( Observable o, Object arg ) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void clear () {
+		// TODO Auto-generated method stub
 	}
 }
