88b420a4723a78697efc040f9b4f873020134ce5
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-30 14:33:23 +0100

    delete unused controller attribute

--- src/view/LogoPanel.java
+++ src/view/LogoPanel.java
@@ -2,8 +2,6 @@ package view;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.util.Observable;
-import java.util.Observer;
 
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -13,18 +11,14 @@ import controller.AppController;
 @SuppressWarnings("serial")
 public class LogoPanel extends JPanel {
 
-	private AppController controller;
-
-	public LogoPanel( AppController controller ) {
+	public LogoPanel ( AppController controller ) {
 		super();
 		this.setBackground( new Color( 0x2F3238 ) );
-		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
-		
-		this.controller = controller;
+		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );		
 	}
 
 	@Override
-	public Dimension getPreferredSize() {
+	public Dimension getPreferredSize () {
 		return new Dimension( 200, 200 );
 	}
 }
