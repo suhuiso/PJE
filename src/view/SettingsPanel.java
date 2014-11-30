3f0ac517100872693a2dcc5aa2351eae45f3d739
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-30 14:56:19 +0100

    update with AbstractCardPanel

--- src/view/SettingsPanel.java
+++ src/view/SettingsPanel.java
@@ -1,21 +1,28 @@
 package view;
 
 import java.awt.Color;
-
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import java.util.Observable;
 
 import controller.AppController;
 
 @SuppressWarnings("serial")
-public class SettingsPanel extends JPanel {
+public class SettingsPanel extends AbstractCardPanel {
 
     public static final String CARD_SETTINGS =  "Settings Panel";
     public static final Color SETTINGS_COLOR = new Color( 0x9E54BD );
 
 	public SettingsPanel( AppController controller ) {
-		super();		
-		this.setBackground( new Color( 0x9E54BD ) );
-		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		super( controller );		
+		this.setBackground( SettingsPanel.SETTINGS_COLOR );
+	}
+
+	@Override
+	public void update ( Observable o, Object arg ) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void clear () {
+		// TODO Auto-generated method stub
 	}
 }
