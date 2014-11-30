01bd705f372d6d86cbbd047801b7af64050f1b0c
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-30 13:27:17 +0100

    compact and update TweetPanel source code

--- src/view/TweetPanel.java
+++ src/view/TweetPanel.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import twitter4j.Status;
 import controller.AppController;
@@ -13,48 +14,26 @@ import controller.AppController;
 @SuppressWarnings("serial")
 public class TweetPanel extends JPanel {
 
-	////////////
-	// FIELDS //
-	////////////
-	
-	/**
-	 * Controller of the TweetPanel
-	 */
 	private AppController controller;
-
-	/**
-	 * Status of the tweet
-	 */
 	private Status status;
 	
-	/////////////
-	// METHODS //
-	/////////////
-	
-	/**
-	 * Constructor of the TweetPanel
-	 * 
-	 * @param controller
-	 *            controller of the TweetPanel
-	 */
 	public TweetPanel( AppController controller, Status status ) {
 		super();
-
-		this.setBackground( Color.WHITE );		/* color: white */
+		this.setBackground( Color.WHITE );
+		this.setBorder( new EmptyBorder( 10, 10, 10, 10 ) );
 		
 		this.setLayout( new BorderLayout() );
 		
-		/* Controller of TweetPanel is added */
 		this.controller = controller;
-		/* Status of this tweet is added */
+		
 		this.status = status;
 		
-		this.add( new JLabel( "@" + status.getUser().getScreenName() ), BorderLayout.NORTH );
-		this.add( new JLabel( status.getText() ), BorderLayout.CENTER );
+		this.add( new JLabel( "@" + this.status.getUser().getScreenName() ), BorderLayout.NORTH );
+		this.add( new JLabel( this.status.getText() ), BorderLayout.CENTER );
 	}
 
 	@Override
 	public Dimension getPreferredSize() {
-		return new Dimension( 400, 60 );
+		return new Dimension( 750, 100 );
 	}
 }
