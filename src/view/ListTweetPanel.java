3429e9226d3af0b4d2a6b5ff0d39342187776449
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-30 13:27:00 +0100

    update ListTweetPanel to the new UI

--- src/view/ListTweetPanel.java
+++ src/view/ListTweetPanel.java
@@ -1,14 +1,14 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.Rectangle;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.Scrollable;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import twitter4j.QueryResult;
@@ -16,43 +16,36 @@ import twitter4j.Status;
 import controller.AppController;
 
 @SuppressWarnings("serial")
-public class ListTweetPanel extends JPanel implements Scrollable, Observer {
+public class ListTweetPanel extends JPanel implements Observer {
 
 	private AppController controller;
-
+	private DefaultListModel<TweetPanel> listTweetModel;
+	private JList<TweetPanel> listTweet;
+	private JScrollPane scrollPane;
+	
 	public ListTweetPanel( AppController controller, Color color ) {
 		super();		
 		this.setBackground( color );
-		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		this.setBorder( new EmptyBorder( 0, 25, 25, 25 ) );
 		
-		this.setLayout( new GridLayout( 0, 1, 0, 1 ) );
+		this.setLayout( new BorderLayout() );
 
 		this.controller = controller;
-	}
-
-	@Override
-	public Dimension getPreferredScrollableViewportSize() {
-		return this.getPreferredSize();
-	}
-
-	@Override
-	public int getScrollableUnitIncrement( Rectangle visibleRect, int orientation, int direction ) {
-		return 5;
-	}
-
-	@Override
-	public int getScrollableBlockIncrement( Rectangle visibleRect, int orientation, int direction ) {
-		return 5;
-	}
-
-	@Override
-	public boolean getScrollableTracksViewportWidth() {
-		return true;
-	}
-
-	@Override
-	public boolean getScrollableTracksViewportHeight() {
-		return false;
+		
+		this.listTweetModel = new DefaultListModel<TweetPanel>();
+		
+		this.listTweet = new JList<TweetPanel>( this.listTweetModel );
+	    this.listTweet.setCellRenderer( new TweetRenderer() );
+		this.listTweet.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		
+		this.scrollPane = new JScrollPane( this.listTweet );		
+		this.scrollPane.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		this.scrollPane.setVerticalScrollBarPolicy( JScrollPane.VERTICAL_SCROLLBAR_ALWAYS );
+		this.scrollPane.setHorizontalScrollBarPolicy( JScrollPane.HORIZONTAL_SCROLLBAR_NEVER );
+		
+		System.out.println( this.scrollPane.getVerticalScrollBar().getUnitIncrement() );
+		
+		this.add( this.scrollPane, BorderLayout.CENTER );
 	}
 
 	@Override
@@ -60,7 +53,7 @@ public class ListTweetPanel extends JPanel implements Scrollable, Observer {
 		QueryResult qr = ( QueryResult ) arg;
 
 		for ( Status status : qr.getTweets() ) {
-			this.add( new TweetPanel( this.controller, status ) );
+			this.listTweetModel.addElement( new TweetPanel( this.controller, status ) );
 		}		
 
 		/* View have changed and need to be repaint */
