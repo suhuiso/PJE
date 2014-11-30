2cd6d8c1be6a0000fdd42c72dfd3040f0a4f6ecd
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-11-30 15:00:07 +0100

    clean SearchButton

--- src/view/SearchButton.java
+++ src/view/SearchButton.java
@@ -1,15 +1,9 @@
 package view;
 
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
 
@@ -23,11 +17,11 @@ public class SearchButton extends JButton {
 		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
 		this.setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
 		
-		this.setOpaque(true);
+		this.setOpaque( true );
 	}
 	
 	@Override
-	public Dimension getPreferredSize() {
+	public Dimension getPreferredSize () {
 		return new Dimension( 150, 100 );
 	}
 }
