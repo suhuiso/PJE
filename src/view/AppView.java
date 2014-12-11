d25bc5be7ac9d55635084d8ec59e71a497c7d763
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-12-11 16:33:08 +0100

    save the TweetPool in the CSV file when the app exit

--- src/view/AppView.java
+++ src/view/AppView.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -11,15 +14,26 @@ import controller.AppController;
 public class AppView extends JFrame implements Observer {
 
 	private AppPanel appPanel;
-
+	private AppController controller;
+	
 	public AppView ( AppController controller ) {
 		this.setTitle( "AwesomeName" );
 		this.setSize( 1000, 600 );
 		this.setResizable( false );		
 		this.setLocationRelativeTo( null );
-		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		this.setDefaultCloseOperation( JFrame.DO_NOTHING_ON_CLOSE );
 		
 		this.appPanel = new AppPanel( controller );
+		this.controller = controller;
+		
+		WindowListener exitListener = new WindowAdapter () {
+			@Override
+			public void windowClosing ( WindowEvent e ) {
+				AppView.this.controller.closingWindowSaveRequest();
+				System.exit(0);
+			}
+		};
+        this.addWindowListener( exitListener );
 		
 		this.setContentPane( appPanel );
 		
