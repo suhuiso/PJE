94f18cc73cd83a25e5119ac008bab50d969e51ee
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-12-11 13:59:43 +0100

    add ClassificationButton

---
+++ src/view/ClassificationButton.java
@@ -0,0 +1,66 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
+
+import utils.Tweet;
+import controller.AppController;
+import feeling.Feeling;
+
+@SuppressWarnings("serial")
+public class ClassificationButton extends JButton {
+
+	private AppController controller;
+	private Feeling feeling;
+	
+	public ClassificationButton ( AppController controller, String text, Feeling feeling ) {
+		super( text );
+		//this.setForeground( Color.WHITE );
+		//this.setBackground( new Color( 0x2F3238 ) );
+		//this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		//this.setFont( new Font( "Lucida Sans", Font.PLAIN, 15 ) );
+		
+		this.controller = controller;
+		this.feeling = feeling;
+		
+		//this.setOpaque( true );
+				
+		this.initListener();
+	}
+	
+	@Override
+	public Dimension getPreferredSize () {
+		return new Dimension( 150, 35 );
+	}
+	
+	public Feeling getFeeling () {
+		return this.feeling;
+	}
+	
+	public void highlight () {
+		this.setBackground( new Color( 0xFCD04B ) );
+		this.repaint();
+	}
+	
+	private void initListener () {
+		ClassificationButtonListener classificationButtonListener = new ClassificationButtonListener();
+		this.addActionListener( classificationButtonListener );
+	}
+	
+	class ClassificationButtonListener implements ActionListener {
+		public void actionPerformed ( ActionEvent e ) {	
+			ClassificationButton buttonPressed = ( ClassificationButton ) e.getSource();
+			LearningTweetPanel learningTweetPanel = ( LearningTweetPanel ) buttonPressed.getParent().getParent();
+			Tweet currentTweet = learningTweetPanel.getTweet();
+			ClassificationButton.this.controller.saveRequest( currentTweet, buttonPressed.getFeeling() );
+			buttonPressed.highlight();
+			System.out.println( "ClassificationButtonListener@actionPerformed: " + buttonPressed.getFeeling() );
+		}
+	}
+}
