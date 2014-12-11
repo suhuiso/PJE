406e2cb651a3e24e9b5f2693520329accbba6c4d
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-12-11 14:00:56 +0100

    now use ClassificationButtons

--- src/view/ClassificationsPanel.java
+++ src/view/ClassificationsPanel.java
@@ -1,28 +1,30 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import controller.AppController;
+import feeling.Feeling;
 
 @SuppressWarnings("serial")
 public class ClassificationsPanel extends JPanel {
 
-	private JButton negativeButton;
-	private JButton neutralButton;
-	private JButton positiveButton;
+	private ClassificationButton negativeButton;
+	private ClassificationButton neutralButton;
+	private ClassificationButton positiveButton;
 	
 	public ClassificationsPanel ( AppController controller ) {
 		super();
 		
+		this.setBackground( Color.RED );
 		this.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
 		
-		this.negativeButton = new JButton( "Negatif" );
-		this.neutralButton = new JButton( "Neutre" );
-		this.positiveButton = new JButton( "Positif" );
+		this.negativeButton = new ClassificationButton( controller, "Negatif", Feeling.NEGATIVE );
+		this.neutralButton = new ClassificationButton( controller, "Neutre", Feeling.NEUTRAL );
+		this.positiveButton = new ClassificationButton( controller, "Positif", Feeling.POSITIVE );
 		
 		this.add( this.negativeButton );
 		this.add( this.neutralButton );
