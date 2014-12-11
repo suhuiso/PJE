cad7c5b491ec22afcc0134eadab03ab539f6c055
qw0te <baert.quentin@gmail.com>
2014-12-11 11:35:44 +0100

    delete useless method (no need to save tweets using a classifier)

--- src/controller/AppController.java
+++ src/controller/AppController.java
@@ -4,8 +4,9 @@ import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import model.AppModel;
-import twitter4j.QueryResult;
+import utils.Tweet;
 import feeling.Classifier;
+import feeling.Feeling;
 
 /**
  * Controller of the application. Make the link between the view and the model.
@@ -13,7 +14,7 @@ import feeling.Classifier;
  * @author Quentin Baert & Thomas Bernard
  */
 public class AppController {
-	
+
 	////////////
 	// FILEDS //
 	////////////
@@ -22,17 +23,17 @@ public class AppController {
 	 * Model linked with the controller.
 	 */
 	protected AppModel model;
-	
+
 	/**
 	 * Preferences of the application
 	 */
 	private Preferences prefs;
-	
+
 	/**
 	 * Current classifier of the application
 	 */
 	private Classifier currentClassifier;
-	
+
 	/////////////
 	// METHODS //
 	/////////////
@@ -46,26 +47,28 @@ public class AppController {
 	public AppController ( AppModel model ) {
 		this.model = model;
 		this.prefs = Preferences.userNodeForPackage( this.getClass() );
-		this.currentClassifier = this.model.getClassifierById( this.prefs.getInt( "CURRENT_ASSIGNER", 0 ) );
+		this.currentClassifier =
+		        this.model.getClassifierById( this.prefs.getInt( "CURRENT_ASSIGNER", 0 ) );
 	}
 
 	public Classifier getCurrentClassifier () {
-		System.out.println( "getCurrentCLassifier : current classifier = " + this.currentClassifier );
+		System.out
+		        .println( "getCurrentCLassifier : current classifier = " + this.currentClassifier );
 		return this.currentClassifier;
 	}
-	
+
 	public void setCurrentClassifierId ( int id ) {
 		System.out.println( "setCurrentClassifierId: current classifier = " + id );
 		this.currentClassifier = this.model.getClassifierById( id );
 		this.prefs.putInt( "CURRENT_ASSIGNER", id );
 		try {
 			this.prefs.flush();
-		} catch (BackingStoreException e) {
+		} catch ( BackingStoreException e ) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Controls the query passed in parameter.
 	 * 
@@ -98,17 +101,7 @@ public class AppController {
 	public void unpolarizedSaveRequest ( QueryResult result ) {
 		this.model.unpolarizedSave( result );
 	}
-	
-	/**
-	 * Asks to the model to save the results of a query with a dictionary method.
-	 * 
-	 * @param result
-	 *            results of a query previously made
-	 */
-	public void dictionarySaveRequest ( QueryResult result ) {
-		this.model.dictionarySave( result );
-	}
-	
+
 	/**
 	 * Asks to the model to save informations that need to be saved before closing the window.
 	 */
