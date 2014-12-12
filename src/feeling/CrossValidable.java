3b6550ef0ae6a6de00b76dd5fe3128d52eac6487
qw0te <baert.quentin@gmail.com>
2014-12-12 21:41:52 +0100

    add a new abstract class CrossValidable representing classifier that can be tested with a cross validation

---
+++ src/feeling/CrossValidable.java
@@ -0,0 +1,39 @@
+package feeling;
+
+import utils.TweetPool;
+
+/**
+ * Abstract class representing classifier that are cross validable.
+ * 
+ * @author Quentin Baert & Thomas Bernard
+ */
+public abstract class CrossValidable extends Classifier {
+
+	////////////
+	// FIELDS //
+	////////////
+
+	/**
+	 * Tweet pool using by the classifier as learning base.
+	 */
+	protected TweetPool tweetPool;
+
+	/////////////
+	// METHODS //
+	/////////////
+	
+	public CrossValidable ( TweetPool tweetPool ) {
+		this.tweetPool = tweetPool;
+	}
+
+	/**
+	 * Sets the tweet pool of the cross validable classifier.
+	 * 
+	 * @param newTweetPool
+	 *            new tweet pool of the cross validable classifier
+	 */
+	public void setTweetPool ( TweetPool newTweetPool ) {
+		this.tweetPool = newTweetPool;
+	}
+
+}
