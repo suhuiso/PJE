c2ae1cdc40124212743e2c04a598a2bfcf590c84
qw0te <baert.quentin@gmail.com>
2014-12-14 13:22:33 +0100

    add classes to evaluate the error rate of a classifier

---
+++ src/statistics/ClassifierStatistics.java
@@ -0,0 +1,15 @@
+package statistics;
+
+/**
+ * Class representing objects that make statistics about a classifier.
+ * 
+ * @author Quentin Baert & Thomas Bernard
+ */
+public abstract class ClassifierStatistics {
+
+	/**
+	 * Evaluates the classifier using the tweet pool.
+	 */
+	public abstract float evaluates ();
+
+}
