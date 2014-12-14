33794c920435a32ce74a514dc12b60196bbd5b02
qw0te <baert.quentin@gmail.com>
2014-12-14 17:00:02 +0100

    add all the classifiers to the view

--- src/view/AssignerSettingsPanel.java
+++ src/view/AssignerSettingsPanel.java
@@ -14,90 +14,83 @@ import javax.swing.JRadioButton;
 
 import controller.AppController;
 
-@SuppressWarnings("serial")
+@SuppressWarnings ( "serial" )
 public class AssignerSettingsPanel extends JPanel {
 
 	private AppController controller;
+
 	private JLabel label;
-	private JRadioButton radio1;
-	private JRadioButton radio2;
-	private JRadioButton radio3;
-	private JRadioButton radio4;
-	private JRadioButton radio5;
+
+	private JRadioButton[] radios;
+
 	private ButtonGroup buttonGroup;
-	
+
 	public AssignerSettingsPanel ( AppController controller, Color color ) {
 		super();
 		this.setBackground( color );
 		this.setLayout( new GridLayout( 0, 1 ) );
-		
+
 		this.controller = controller;
 		this.label = new JLabel( "Classifieurs disponibles :" );
-		this.radio1 = new JRadioButton( "Dictionnaire" );
-		this.radio2 = new JRadioButton( "KNN" );
-		this.radio3 = new JRadioButton( "Bayes par présence" );
-		this.radio4 = new JRadioButton( "Bayes par fréquence" );
-		this.radio5 = new JRadioButton( "Bayes par fréquence uni-gramme" );
+		this.radios = new JRadioButton[ 14 ];
+
+		this.radios[ 0 ] = new JRadioButton( "Dictionnaire" );
+		this.radios[ 1 ] = new JRadioButton( "KNN" );
+		this.radios[ 2 ] = new JRadioButton( "Bayes : présence, non simplifié, uni-grammes" );
+		this.radios[ 3 ] = new JRadioButton( "Bayes : présence, simplifié, uni-grammes" );
+		this.radios[ 4 ] = new JRadioButton( "Bayes : présence, non simplifié, bi-grammes" );
+		this.radios[ 5 ] = new JRadioButton( "Bayes : présence, simplifié, uni-grammes" );
+		this.radios[ 6 ] =
+		        new JRadioButton( "Bayes : présence, non simplifié, uni-grammes + bi-grammes" );
+		this.radios[ 7 ] =
+		        new JRadioButton( "Bayes : présence, simplifié, uni-grammes + bi-grammes" );
+		this.radios[ 8 ] = new JRadioButton( "Bayes : fréquence, non simplifié, uni-grammes" );
+		this.radios[ 9 ] = new JRadioButton( "Bayes : fréquence, simplifié, uni-grammes" );
+		this.radios[ 10 ] = new JRadioButton( "Bayes : fréquence, non simplifié, bi-grammes" );
+		this.radios[ 11 ] = new JRadioButton( "Bayes : fréquence, simplifié, uni-grammes" );
+		this.radios[ 12 ] =
+		        new JRadioButton( "Bayes : fréquence, non simplifié, uni-grammes + bi-grammes" );
+		this.radios[ 13 ] =
+		        new JRadioButton( "Bayes : fréquence, simplifié, uni-grammes + bi-grammes" );
+
 		this.buttonGroup = new ButtonGroup();
 
-		this.buttonGroup.add( this.radio1 );
-		this.buttonGroup.add( this.radio2 );
-		this.buttonGroup.add( this.radio3 );
-		this.buttonGroup.add( this.radio4 );
-		this.buttonGroup.add( this.radio5 );
-
 		this.label.setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
-		this.radio1.setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
-		this.radio2.setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
-		this.radio3.setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
-		this.radio4.setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
-		this.radio5.setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
-
 		this.add( this.label );
-		this.add( this.radio1 );
-		this.add( this.radio2 );
-		this.add( this.radio3 );
-		this.add( this.radio4 );
-		this.add( this.radio5 );
-				
-		//int currentAssigner = this.controller.getCurrentClassifier();
-		
-		/*if ( currentAssigner == 0 ) {
-			this.prefs.putInt( "CURRENT_ASSIGNER", 1 );
-			this.radio1.setSelected( true );
-		} else {
-			// TODO select le classifieur courant
-		}*/
-		
+
+		for ( int i = 0; i < this.radios.length; i++ ) {
+			this.radios[ i ].setFont( new Font( "Lucida Sans", Font.PLAIN, 18 ) );
+			this.buttonGroup.add( this.radios[ i ] );
+			this.add( this.radios[ i ] );
+		}
+
 		this.initListeners();
 	}
-	
+
 	private void initListeners () {
-		this.radio1.addActionListener( new RadioButtonListener( 0 ) );
-		this.radio2.addActionListener( new RadioButtonListener( 1 ) );
-		this.radio3.addActionListener( new RadioButtonListener( 2 ) );
-		this.radio4.addActionListener( new RadioButtonListener( 3 ) );
-		this.radio5.addActionListener( new RadioButtonListener( 4 ) );
+		for ( int i = 0; i < this.radios.length; i++ ) {
+			this.radios[ i ].addActionListener( new RadioButtonListener( i ) );
+		}
 	}
-	
+
 	@Override
 	public Dimension getPreferredSize () {
-		return new Dimension( 750, 200 );
+		return new Dimension( 750, 400 );
 	}
 
 	@Override
 	public Dimension getMaximumSize () {
-		return new Dimension( 750, 200 );
+		return new Dimension( 750, 400 );
 	}
-	
+
 	class RadioButtonListener implements ActionListener {
-		
+
 		private int assignerUsed;
-		
+
 		public RadioButtonListener ( int assignerUsed ) {
 			this.assignerUsed = assignerUsed;
 		}
-		
+
 		public void actionPerformed ( ActionEvent e ) {
 			AssignerSettingsPanel.this.controller.setCurrentClassifierId( this.assignerUsed );
 		}
