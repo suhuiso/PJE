c170a02a425f14bce56093365d141507902db587
Thomas Bernard <thomas.t.bernard@gmail.com>
2014-12-15 02:17:48 +0100

    begins EvaluationPanel content

--- src/view/EvaluationPanel.java
+++ src/view/EvaluationPanel.java
@@ -1,7 +1,15 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.util.Observable;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 import controller.AppController;
 
@@ -12,9 +20,37 @@ public class EvaluationPanel extends AbstractCardPanel {
     public static final Color EVALUATION_COLOR = new Color( 0xF06060 );
     public static final Color EVALUATION_COLOR_BRIGHTER = new Color( 0xF06060 );
     
+    private AppController controller;
+    private EvaluationTitlePanel evaluationTitlePanel;
+    private JLabel evaluationValueLabel;
+    private JButton evaluationButton;
+    
 	public EvaluationPanel( AppController controller ) {
 		super( controller );
 		this.setBackground( EvaluationPanel.EVALUATION_COLOR );
+		this.setBorder( new EmptyBorder( 25, 25, 25, 25 ) );
+		
+		this.setLayout( new BorderLayout() );
+		
+		this.controller = controller;
+		
+		this.evaluationTitlePanel = new EvaluationTitlePanel( controller, EvaluationPanel.EVALUATION_COLOR );
+		
+		this.evaluationValueLabel = new JLabel( "53%", SwingConstants.CENTER );
+		this.evaluationValueLabel.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		this.evaluationValueLabel.setFont( new Font( "Lucida Sans", Font.PLAIN, 100 ) );
+		
+		this.evaluationButton = new JButton( "Evaluer" );
+		this.evaluationButton.setForeground( Color.WHITE );
+		this.evaluationButton.setBackground( new Color( 0x2F3238 ) );
+		this.evaluationButton.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		this.evaluationButton.setFont( new Font( "Lucida Sans", Font.PLAIN, 20 ) );
+		this.evaluationButton.setPreferredSize( new Dimension( 150, 100 ) );
+		this.evaluationButton.setOpaque( true );
+		
+		this.add( this.evaluationTitlePanel, BorderLayout.NORTH );
+		this.add( this.evaluationValueLabel, BorderLayout.CENTER );
+		this.add( this.evaluationButton, BorderLayout.SOUTH );
 	}
 
 	public void update(Observable o, Object arg) {
---
+++ src/view/EvaluationTitlePanel.java
@@ -0,0 +1,50 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import controller.AppController;
+import feeling.Classifier;
+
+@SuppressWarnings("serial")
+public class EvaluationTitlePanel extends JPanel {
+
+	private AppController controller;
+	private Classifier classifier;
+	private JLabel evaluationLabel;
+	private JLabel classifierLabel;
+	
+	public EvaluationTitlePanel( AppController controller, Color color ) {
+		super();
+		this.setBackground( color );
+		this.setBorder( new EmptyBorder( 50, 0, 0, 0 ) );
+		
+		this.setLayout( new BorderLayout() );
+		
+		this.controller = controller;
+		this.classifier = this.controller.getCurrentClassifier();
+		
+		this.evaluationLabel = new JLabel( "Evaluation du classifieur :", SwingConstants.CENTER );
+		this.evaluationLabel.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		this.evaluationLabel.setBackground( color );
+		this.evaluationLabel.setForeground( new Color( 0x2F3238 ) );
+		this.evaluationLabel.setFont( new Font( "Lucida Sans", Font.PLAIN, 20 ) );
+		this.evaluationLabel.setOpaque( true );		
+		
+		this.classifierLabel = new JLabel( this.classifier.toString(), SwingConstants.CENTER );
+		this.classifierLabel.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
+		this.classifierLabel.setBackground( color );
+		this.classifierLabel.setForeground( new Color( 0x2F3238 ) );
+		this.classifierLabel.setFont( new Font( "Lucida Sans", Font.BOLD, 20 ) );
+		this.classifierLabel.setOpaque( true );	
+		
+		this.add( this.evaluationLabel, BorderLayout.NORTH );
+		this.add( this.classifierLabel, BorderLayout.SOUTH );
+	}
+}
