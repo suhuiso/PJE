e8d62fbd03a9d163c39399fac03ae5c3f7075884
qw0te <baert.quentin@gmail.com>
2014-12-15 19:07:32 +0100

    use a decimal formater to print the cross validation result

--- src/view/EvaluationPanel.java
+++ src/view/EvaluationPanel.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import java.util.Observable;
 
 import javax.swing.BorderFactory;
@@ -17,72 +18,81 @@ import javax.swing.border.EmptyBorder;
 import controller.AppController;
 import feeling.Classifier;
 
-@SuppressWarnings("serial")
+@SuppressWarnings ( "serial" )
 public class EvaluationPanel extends AbstractCardPanel {
 
-    public static final String CARD_EVALUATION =  "Evaluation Panel";
-    public static final Color EVALUATION_COLOR = new Color( 0xF06060 );
-    public static final Color EVALUATION_COLOR_BRIGHTER = new Color( 0xF06060 );
-    
-    private AppController controller;
+	public static final String CARD_EVALUATION = "Evaluation Panel";
+
+	public static final Color EVALUATION_COLOR = new Color( 0xF06060 );
+
+	public static final Color EVALUATION_COLOR_BRIGHTER = new Color( 0xF06060 );
+
+	private AppController controller;
+
 	private Classifier classifier;
-    private EvaluationTitlePanel evaluationTitlePanel;
-    private JLabel evaluationValueLabel;
-    private JButton evaluationButton;
-    
-	public EvaluationPanel( AppController controller ) {
+
+	private EvaluationTitlePanel evaluationTitlePanel;
+
+	private JLabel evaluationValueLabel;
+
+	private JButton evaluationButton;
+
+	public EvaluationPanel ( AppController controller ) {
 		super( controller );
 		this.setBackground( EvaluationPanel.EVALUATION_COLOR );
 		this.setBorder( new EmptyBorder( 25, 25, 25, 25 ) );
-		
+
 		this.setLayout( new BorderLayout() );
-		
+
 		this.controller = controller;
 		this.classifier = this.controller.getCurrentClassifier();
-		
-		this.evaluationTitlePanel = new EvaluationTitlePanel( controller, EvaluationPanel.EVALUATION_COLOR );
-		
+
+		this.evaluationTitlePanel =
+		        new EvaluationTitlePanel( controller, EvaluationPanel.EVALUATION_COLOR );
+
 		this.evaluationValueLabel = new JLabel( "", SwingConstants.CENTER );
 		this.evaluationValueLabel.setBorder( new EmptyBorder( 0, 0, 0, 0 ) );
 		this.evaluationValueLabel.setFont( new Font( "Lucida Sans", Font.PLAIN, 100 ) );
-		
+
 		this.evaluationButton = new JButton( "Evaluer" );
-		if ( !( this.controller.getCurrentClassifier().isCrossValidable() ) ) {
+		if ( ! ( this.controller.getCurrentClassifier().isCrossValidable() ) ) {
 			this.evaluationButton.setText( "Evaluation impossible sur ce classifieur" );
 			this.evaluationButton.setEnabled( false );
 		}
 		this.evaluationButton.setForeground( Color.WHITE );
 		this.evaluationButton.setBackground( new Color( 0x2F3238 ) );
-		this.evaluationButton.setBorder( BorderFactory.createMatteBorder( 15, 150, 15, 150, EvaluationPanel.EVALUATION_COLOR ) );
+		this.evaluationButton.setBorder( BorderFactory.createMatteBorder( 15, 150, 15, 150,
+		        EvaluationPanel.EVALUATION_COLOR ) );
 		this.evaluationButton.setFont( new Font( "Lucida Sans", Font.PLAIN, 20 ) );
 		this.evaluationButton.setPreferredSize( new Dimension( 150, 100 ) );
 		this.evaluationButton.setOpaque( true );
-		
+
 		this.add( this.evaluationTitlePanel, BorderLayout.NORTH );
 		this.add( this.evaluationValueLabel, BorderLayout.CENTER );
 		this.add( this.evaluationButton, BorderLayout.SOUTH );
-		
+
 		this.initListeners();
 	}
 
 	public void update ( Observable o, Object arg ) {
-		Float value = ( Float ) arg;
-		this.evaluationValueLabel.setText( value + " %" );
+		DecimalFormat df = new DecimalFormat( "###.##" );
+		Double value = ( Double ) arg;
+		this.evaluationValueLabel.setText( df.format( value ) + " %" );
 	}
 
 	public void clear () {
 		this.evaluationValueLabel.setText( "" );
 	}
-	
+
 	public String toString () {
 		return EvaluationPanel.CARD_EVALUATION;
 	}
 
 	@Override
-	public void resume() {
+	public void resume () {
 		this.classifier = this.controller.getCurrentClassifier();
 		this.evaluationTitlePanel.resume();
-		if ( !( this.classifier.isCrossValidable() ) ) {
+		if ( ! ( this.classifier.isCrossValidable() ) ) {
 			this.evaluationButton.setText( "Evaluation impossible sur ce classifieur" );
 			this.evaluationButton.setEnabled( false );
 		} else {
@@ -90,13 +100,14 @@ public class EvaluationPanel extends AbstractCardPanel {
 			this.evaluationButton.setEnabled( true );
 		}
 	}
-	
+
 	private void initListeners () {
 		EvaluationButtonListener EvaluationButtonListener = new EvaluationButtonListener();
 		this.evaluationButton.addActionListener( EvaluationButtonListener );
 	}
-	
+
 	class EvaluationButtonListener implements ActionListener {
+
 		public void actionPerformed ( ActionEvent e ) {
 			EvaluationPanel.this.controller.evaluateCurrentClassifier();
 		}
